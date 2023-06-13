62c0bf0fdb490f1876dcd4ea3c1c969d1e94bf90
Kaan Genis <107223151+kaangenis@users.noreply.github.com>
2023-06-13 22:55:21 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import LoginPage from './src/screens/LoginPage'
-import SecondPage from './src/screens/SecondPage'
+import LoginPage from './src/LoginPage'
+import RegisterPage from './src/RegisterPage'
 import HomeScreen from './src/screens/HomeScreen'
 import ThirdPage from './src/screens/ThirdPage'
 import FourthPage from './src/screens/FourthPage'
@@ -14,7 +14,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown : false}}>
         <Stack.Screen name = 'LoginPage' component={LoginPage}></Stack.Screen>
-        <Stack.Screen name = 'SecondPage' component={SecondPage}></Stack.Screen>
+        <Stack.Screen name = 'RegisterPage' component={RegisterPage}></Stack.Screen>
         <Stack.Screen name = 'HomeScreen' component={HomeScreen}></Stack.Screen>
         <Stack.Screen name = 'ThirdPage' component={ThirdPage}></Stack.Screen>
         <Stack.Screen name = 'FourthPage' component={FourthPage}></Stack.Screen>
